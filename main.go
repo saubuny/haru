82418b937f692d9b7d9e2aecfb5ee134c8f8c938
saubuny <saubuny@gmail.com>
2024-10-29 09:09:20 -0400

    add cli functionality

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"context"
 	_ "embed"
+	"log"
+	"os"
+
+	"github.com/saubuny/haru/internal/database"
+
 	tea "github.com/charmbracelet/bubbletea"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/saubuny/haru/internal/database"
-	"log"
+	"github.com/urfave/cli/v2"
 )
 
 type dbConfig struct {
@@ -17,14 +21,32 @@ type dbConfig struct {
 //go:embed sql/schema/schema.sql
 var migrations string
 
-// TODO
+// === TODO ===
 // 1. CLI interaction
 // 2. Display DB information in TUI table (have different tabs?)
 
 func main() {
-	cfg, err := initDB(migrations)
+	_, err := initDB(migrations)
 	if err != nil {
 		log.Fatalf("Error initalizing DB: %v", err)
+	}
+
+	// Run TUI by default
+	app := &cli.App{
+		Name:  "Haru",
+		Usage: "Track anime",
+		Action: func(ctx *cli.Context) error {
+			p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+			tea.SetWindowTitle("Haru")
+			if _, err := p.Run(); err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 
 	// cfg.importMAL()
@@ -38,10 +60,4 @@ func main() {
 	// if err != nil {
 	// 	log.Fatalf("Error: %v", err)
 	// }
-
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
-	tea.SetWindowTitle("Haru")
-	if _, err := p.Run(); err != nil {
-		log.Fatalf("Error: %v", err)
-	}
 }
